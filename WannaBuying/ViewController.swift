e50b7cda8c8372065c1b066cb344c4ebd2cd3d2d
s92104 <s92104@yahoo.com.tw>
2019-04-28 06:17:03 -0700

    Initial Commit

---
+++ WannaBuying/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WannaBuying
+//
+//  Created by s92104 on 2019/4/28.
+//  Copyright © 2019 s92104. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
